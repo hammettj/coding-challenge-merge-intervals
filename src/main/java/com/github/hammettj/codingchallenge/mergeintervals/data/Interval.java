868945f2e3d0f2d9363a5109bebfcf792cc1b871
hammettj <hammettj 67120676+hammettj@users.noreply.github.com>
2022-01-11 18:25:46 +0100

    make instances of Interval comparable

--- src/main/java/com/github/hammettj/codingchallenge/mergeintervals/data/Interval.java
+++ src/main/java/com/github/hammettj/codingchallenge/mergeintervals/data/Interval.java
@@ -1,12 +1,14 @@
 package com.github.hammettj.codingchallenge.mergeintervals.data;
 
+import static java.lang.Integer.compare;
+
 /**
  * Representation of an interval.
  *
  * <p>An interval is composed of {@link Interval#begin} and {@link Interval#end} where {@link Interval#end}
  * must be greater than {@link Interval#begin}.</p>
  */
-public record Interval(int begin, int end) {
+public record Interval(int begin, int end) implements Comparable<Interval> {
     /**
      * Creates a new instance of this class.
      *
@@ -21,6 +23,12 @@ public record Interval(int begin, int end) {
         if (begin > end) {
             throw new IllegalArgumentException("begin must be less than end");
         }
+    }
+
+    @Override
+    public int compareTo(final Interval interval) {
+        final int res = compare(begin, interval.begin);
+        return (res != 0) ? res : compare(end, interval.end);
     }
 
     @Override
--- src/test/java/com/github/hammettj/codingchallenge/mergeintervals/data/IntervalTest.java
+++ src/test/java/com/github/hammettj/codingchallenge/mergeintervals/data/IntervalTest.java
@@ -2,6 +2,9 @@ package com.github.hammettj.codingchallenge.mergeintervals.data;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class IntervalTest {
@@ -18,5 +21,17 @@ class IntervalTest {
         assertThatThrownBy(() -> new Interval(30, 10))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("begin must be less than end");
+    }
+
+    @Test
+    void comparingIntervalsSortsAscendingByBeginAndEnd() {
+        final Interval interval1 = new Interval(7, 11);
+        final Interval interval2 = new Interval(1, 5);
+        final Interval interval3 = new Interval(2, 6);
+        final Interval interval4 = new Interval(2, 4);
+        final List<Interval> intervals = List.of(interval1, interval2, interval3, interval4);
+
+        assertThat(intervals.stream().sorted())
+            .containsExactly(interval2, interval4, interval3, interval1);
     }
 }
